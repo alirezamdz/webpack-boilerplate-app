81801f41246d69b7b41d1820566e8c200578c47c
Alireza Mehdizadeh <alirezamdz@hotmail.com>
2017-05-14 16:20:31 +0200

    Initial assets and template

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import './stylesheets/vendors/font-awesome.css'
+import './stylesheets/vendors/new-age.css'
+import './stylesheets/style.styl'
+
+import './javascripts/vendors/jquery-easing-1.3.js'
+import './javascripts/vendors/new-age'
+
+
+console.log("Hello from web pack")
