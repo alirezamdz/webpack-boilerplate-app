a432bb393c4ecea27f5a02ad5c7efc5c3f6d644a
Alireza Mehdizadeh <alirezamdz@hotmail.com>
2017-05-14 16:37:06 +0200

    edit title

--- config/webpackHtmlPlugin.js
+++ config/webpackHtmlPlugin.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = new HtmlWebpackPlugin({
-  title: 'Schutz Pro',
+  title: 'Hello webpack',
   minify: {
     collapseWhitespace: true
   },
